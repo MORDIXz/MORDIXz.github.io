a6c930fa815749dc7728b2f0d05cc1f21b77e51a
MORDIXz <2000carmariana2000@gmail.com>
2026-09-19 17:18:15 -0600

    Add files via upload

---
+++ PaginasAlternas/Estudio_Mercado/Estudio_Mercado.js
@@ -0,0 +1,28 @@
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+
+    const urlPdf = 'MKT_II_M1_PF-4_(DUHD).pdf'; // Asegúrate de que la ruta sea correcta
+    const contenedorPdf = document.getElementById('pdf-viewer');
+
+    pdfjsLib.getDocument(urlPdf).promise.then(async (pdfDoc) => {
+        const totalPaginas = pdfDoc.numPages;
+
+        for (let numPagina = 1; numPagina <= totalPaginas; numPagina++) {
+            const pagina = await pdfDoc.getPage(numPagina);
+            const canvas = document.createElement('canvas');
+            const contexto = canvas.getContext('2d');
+            
+            const viewport = pagina.getViewport({ scale: 1.5 }); // Escala de nitidez
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            await pagina.render({
+                canvasContext: contexto,
+                viewport: viewport
+            }).promise;
+
+            contenedorPdf.appendChild(canvas);
+        }
+    }).catch(error => {
+        console.error("Error al cargar el PDF:", error);
+    });
+
---
+++ pascua.js
@@ -0,0 +1,43 @@
+const contenedor = document.getElementById('trigger-secreto');
+    const enlace = document.getElementById('link-secreto');
+    const contrasenaSecreta ="mordix";
+    let contadorClics = 0;
+    let tiempoReseteo = null;
+
+    contenedor.addEventListener('click', function(e) {
+        // Evitamos que el enlace navegue por defecto
+        e.preventDefault();
+        
+        contadorClics++;
+
+        clearTimeout(tiempoReseteo);
+        tiempoReseteo = setTimeout(() => {
+            contadorClics = 0;
+        }, 1000); // 1 segundo de límite
+
+        // Al llegar a los 5 clics seguidos
+        if (contadorClics === 5) {
+            contadorClics = 0;
+            clearTimeout(tiempoReseteo);
+
+            alert('¡Wow Has encontrado mi pagina secreta!');
+            let respuesta = prompt("Te doy una pista: ¿Cuál es el nombre de tu marca de manzanas favoritas?");
+
+            if (respuesta !==null){
+                if(respuesta.toLowerCase().trim() ==contrasenaSecreta){
+                    alert("!Felicidades contraseña correcta, puedes pasar!")
+                    window.location.href = "pascua.html";
+                }else{
+                    alert("Ups contraseña incorrecta")
+                }
+            }
+            
+        }
+    });
+
+
+
+
+
+
+    
